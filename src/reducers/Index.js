66970d3b281d968ddf31b6250614e3a91d6a1568
king-hacki <king-hacki@king-hackis-MacBook-Pro.local>
2020-09-07 10:13:06 +0300

    reducer Index

--- src/reducers/Index.js
+++ src/reducers/Index.js
@@ -1,5 +1,14 @@
 import { combineReducers } from "redux";
+import MarathonReducer from "../components/admin/marathon/MarathonReducer";
+import SprintReducer from "../components/admin/sprint/SprintReducer";
+import TaskReducer from "../components/admin/task/TaskReducer";
+import UserReducer from "../components/admin/user/UserReducer";
+import ProgressReducer from "../components/admin/progress/ProgressReducer";
 
 export default combineReducers({
-
+    MarathonReducer,
+    SprintReducer,
+    TaskReducer,
+    UserReducer,
+    ProgressReducer
 });
